01fac17fd4879d0106d14f48390682ac886415d7
gouravlala <gouravlala001@gmai.com>
2023-01-13 19:23:42 +0530

    alldone

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "yTIwcTnBFP55XimEegVye+Wo+q8MSWTrb5mqMY/O3Vg=",
+    EMAIL:"dagmar.hartmann73@ethereal.email",
+    PASSWORD:"jNaZFvwDcNK118fqTZ" ,
+    ATLAS_URI:"mongodb+srv://admin:admin123@cluster0.avcw7vc.mongodb.net/?retryWrites=true&w=majority"
+}
